e59349c921e04e5b4b7b5c2053de15c75adb4518
Ramanathan Vishnampet <ramanathan.vishnampet@exxonmobil.com>
2021-05-20 19:59:03 +0000

    Added hyperparameter tuning with hyperopt.

--- train.py
+++ train.py
@@ -1,9 +1,20 @@
 """Script for training a regression model on the Ames Housing dataset."""
 from operator import itemgetter
+from typing import Any, Dict, List, Tuple
 
+import hyperopt
+import matplotlib.pyplot as plt
 import mlflow
 import numpy as np
 import pandas as pd
+import seaborn as sns
+from hpsklearn.components import (
+    ada_boost_regression,
+    elasticnet,
+    gradient_boosting_regression,
+    random_forest_regression,
+)
+from hyperopt import hp
 from sklearn import (
     impute,
     linear_model,
@@ -11,11 +22,15 @@ from sklearn import (
     pipeline,
     preprocessing,
 )
+from sklearn.base import BaseEstimator
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.utils._testing import ignore_warnings
 from sklearn_pandas import DataFrameMapper
-from skopt import BayesSearchCV
 
 
-def create_mappper(dataset: pd.DataFrame, label: str) -> DataFrameMapper:
+def create_mappper(
+    dataset: pd.DataFrame, label: str, use_one_hot_encoding: bool = False
+) -> DataFrameMapper:
     """
     Returns a mapper object that transforms a pandas dataframe to numpy arrays
     suitable for use in sklearn estimators.
@@ -52,8 +67,10 @@ def create_mappper(dataset: pd.DataFrame, label: str) -> DataFrameMapper:
         .union(numerical_features)
         .union(set(map(itemgetter(0), ordinal_features)))
     )
-    mapper = DataFrameMapper(
-        [
+
+    features = []
+    for feature in numerical_features:
+        features.append(
             (
                 [feature],
                 [
@@ -61,9 +78,9 @@ def create_mappper(dataset: pd.DataFrame, label: str) -> DataFrameMapper:
                     preprocessing.RobustScaler(),
                 ],
             )
-            for feature in numerical_features
-        ]
-        + [
+        )
+    for feature, categories in ordinal_features:
+        features.append(
             (
                 [feature],
                 [
@@ -72,23 +89,56 @@ def create_mappper(dataset: pd.DataFrame, label: str) -> DataFrameMapper:
                     preprocessing.RobustScaler(),
                 ],
             )
-            for feature, categories in ordinal_features
-        ]
-        + [
-            (
-                [feature],
-                [
-                    impute.SimpleImputer(strategy="most_frequent"),
-                    preprocessing.OneHotEncoder(
-                        drop="if_binary",
-                        categories=[dataset[feature].unique().tolist()],
-                    ),
-                ],
+        )
+    for feature in categorical_features:
+        categories = [dataset[feature].unique().tolist()]
+        if use_one_hot_encoding:
+            features.append(
+                (
+                    [feature],
+                    [
+                        impute.SimpleImputer(strategy="most_frequent"),
+                        preprocessing.OneHotEncoder(
+                            categories=categories, drop="if_binary"
+                        ),
+                    ],
+                )
             )
-            for feature in categorical_features
-        ]
-    )
+        else:
+            features.append(
+                (
+                    [feature],
+                    [
+                        impute.SimpleImputer(strategy="most_frequent"),
+                        preprocessing.OrdinalEncoder(categories=categories),
+                        preprocessing.RobustScaler(),
+                    ],
+                )
+            )
+    mapper = DataFrameMapper(features)
     return mapper
+
+
+def plot_feature_importances(
+    estimator: BaseEstimator,
+    feature_names: List[str],
+    n_top: int = 10,
+    filename: str = "feature_importances.png",
+    figsize: Tuple[float, float] = (10, 8),
+) -> None:
+    """Plot the importances of the `n_top` most important features."""
+
+    _ = plt.figure(figsize=figsize)
+    feature_importances = pd.DataFrame(
+        estimator.feature_importances_, index=feature_names, columns=["importance"]
+    )
+    feature_importances.sort_values("importance", ascending=False, inplace=True)
+    feature_importances = feature_importances.head(n_top)
+    sns.barplot(x=feature_importances["importance"], y=feature_importances.index)
+    plt.title("Random forest feature importance")
+    plt.xlabel("Feature importance")
+    plt.ylabel("Feature names")
+    plt.savefig(filename)
 
 
 def main() -> None:
@@ -96,26 +146,49 @@ def main() -> None:
 
     np.random.seed(0)
     mlflow.sklearn.autolog()
-    dataset = pd.read_csv("data/train.csv")
-    label = "SalePrice"
-    pipe = pipeline.Pipeline(
-        [
-            ("mapper", create_mappper(dataset, label)),
-            ("elastic_net", linear_model.ElasticNet()),
-        ]
-    )
-    params = {
-        "elastic_net__alpha": (1e-4, 100.0, "log-uniform"),
-        "elastic_net__l1_ratio": (0.0, 1.0, "uniform"),
-    }
-    bayes_search = BayesSearchCV(
-        pipe, params, n_iter=5, n_jobs=-1, return_train_score=True
-    )
+    dataset: pd.DataFrame = pd.read_csv("data/train.csv")
+    dataset.drop(columns=["Id"], inplace=True)
     train_dataset, test_dataset = model_selection.train_test_split(
         dataset, test_size=0.2
     )
-    bayes_search.fit(train_dataset, train_dataset[label])
-    bayes_search.score(test_dataset, test_dataset[label])
+    label = "SalePrice"
+
+    def make_pipeline(regressor: BaseEstimator) -> pipeline.Pipeline:
+        mapper = create_mappper(
+            dataset,
+            label,
+            use_one_hot_encoding=isinstance(regressor, linear_model.ElasticNet),
+        )
+        return pipeline.Pipeline([("mapper", mapper), ("regressor", regressor)])
+
+    def objective(regressor: BaseEstimator) -> Dict[str, Any]:
+        pipe = make_pipeline(regressor)
+        with ignore_warnings(category=ConvergenceWarning):
+            scores = model_selection.cross_val_score(
+                pipe, train_dataset, train_dataset[label], scoring="r2"
+            )
+        return {"loss": -scores.mean(), "status": hyperopt.STATUS_OK}
+
+    search_space = hp.choice(
+        "regressor",
+        [
+            elasticnet("elastic_net", max_iter=1000),
+            random_forest_regression("random_forest"),
+            ada_boost_regression("ada_boost"),
+            gradient_boosting_regression("grad_boosting"),
+        ],
+    )
+    result = hyperopt.fmin(
+        fn=objective, space=search_space, algo=hyperopt.tpe.suggest, max_evals=10
+    )
+    pipe = make_pipeline(hyperopt.space_eval(search_space, result))
+    with ignore_warnings(category=ConvergenceWarning):
+        pipe.fit(train_dataset, train_dataset[label])
+    r2_test = pipe.score(test_dataset, test_dataset[label])
+    r2_train = pipe.score(train_dataset, train_dataset[label])
+    print(r2_train, r2_test)
+    if hasattr(pipe.steps[1][1], "feature_importances_"):
+        plot_feature_importances(pipe.steps[1][1], pipe.steps[0][1].transformed_names_)
 
 
 if __name__ == "__main__":
